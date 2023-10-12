bdf8f53caee6b14e226d2db3e5575688a6a73269
julienshim <25723428+julienshim@users.noreply.github.com>
2023-10-11 21:15:59 -0700

    Draw hit boxes

--- tessorocr/main.py
+++ tessorocr/main.py
@@ -12,9 +12,32 @@ with PyTessBaseAPI(path='./tessdata/', lang='kor+eng') as api:
             test_image = test_image.filter(ImageFilter.UnsharpMask())
             new_size = (test_image.size[0] * 2, test_image.size[1] * 2)
             test_image = test_image.resize(new_size)
-            api.SetImage(test_image)
-            test_image.show()
+            # api.SetImage(test_image)
+            # test_image.show()
 
-            utf8_text = api.GetUTF8Text()
-            all_world_confidences = api.AllWordConfidences()
+            # utf8_text = api.GetUTF8Text()
+            # all_world_confidences = api.AllWordConfidences()
+
+            # print(utf8_text)
+            # print(all_world_confidences)
+
+            # api.SetImage(test_image)
+            # boxes = api.GetComponentImages(RIL.TEXTLINE, True)
+            # print('Found {} textline image components.'.format(len(boxes)))
+
+            draw = ImageDraw.Draw(test_image)
+
+            for i, (im, box, _, _) in enumerate(boxes):
+                # im is a PIL image object
+                # box is a dict with x, y, w and h keys
+                api.SetRectangle(box['x'], box['y'], box['w'], box['h'])
+
+                draw.rectangle((box['x'], box['y'], box['x']+ box['w'], box['y'] + box['h']), outline='red', width=5)
+
+                ocrResult = api.GetUTF8Text()
+                conf = api.MeanTextConf()
+                print(u"Box[{0}]: x={x}, y={y}, w={w}, h={h}, "
+                    "confidence: {1}, text: {2}".format(i, conf, ocrResult, **box))
+                
+            test_image.show()
 
