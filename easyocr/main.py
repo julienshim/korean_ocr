978f48612c40dce32168fbfa5efcfc54edd0b80c
julienshim <25723428+julienshim@users.noreply.github.com>
2023-10-16 22:31:45 -0700

    full book run

--- easyocr/main.py
+++ easyocr/main.py
@@ -1,22 +1,27 @@
 import easyocr
 import cv2
 from matplotlib import pyplot as plt
+from os import listdir, path
 
 reader = easyocr.Reader(['ko', 'en'])
-image_path = '../test_data/jpeg/korean-grammar-in-use-beginner/korean-grammar-in-use-beginner-186.jpg'
-result = reader.readtext(image_path)
+test_folder = '../test_data/jpeg/'
+image_paths = list(filter(lambda x: x.endswith('.jpg'), [i for i in listdir(test_folder)]))
+image_paths = list(map(lambda x: path.join(test_folder, x), image_paths))
 
+for image_path in image_paths:
+    image_path = image_path.replace(' ', '\ ')
+    result = reader.readtext(image_path)
 
+    img = cv2.imread(image_path)
 
-img = cv2.imread(image_path)
+    for r in result:
+        # top_left = list(map(lambda x: int(x), r[0][0]))
+        # bottom_right = list(map(lambda x: int(x), r[0][2]))
+        text = r[1] 
+        print(text)
+        # font = cv2.FONT_HERSHEY_SIMPLEX
+        # img = cv2.rectangle(img, top_left, bottom_right, (0,255,0), 5)
+        # img = cv2.putText(img, text, top_left, font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
 
-for r in result:
-    top_left = list(map(lambda x: int(x), r[0][0]))
-    bottom_right = list(map(lambda x: int(x), r[0][2]))
-    text = r[1] 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    img = cv2.rectangle(img, top_left, bottom_right, (0,255,0), 5)
-    img = cv2.putText(img, text, top_left, font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
-
-plt.imshow(img)
-plt.show()
+    plt.imshow(img)
+    plt.show()
